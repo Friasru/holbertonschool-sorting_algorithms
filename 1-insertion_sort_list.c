9db7cdbcac54b07872297ceb1b9be6a863300b19
Friasru <albertofriasru@gmail.com>
2026-04-24 09:23:22 -0700

    insertion sort for doubly linked list that swaps nodes to sort in ascending order

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,41 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a doubly linked list
+ * @list: pointer to the head of the list
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *prev, *next;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+	return;
+
+	current = (*list)->next;
+	while (current != NULL)
+	{
+		next = current->next;
+		prev = current->prev;
+
+		while (prev != NULL && prev->n > current->n)
+		{
+			if (prev->prev != NULL)
+				prev->prev->next = current;
+			else
+				*list = current;
+			
+			current->prev = prev->prev;
+			prev->next = current->next;
+
+			if (current->next != NULL)
+				current->next->prev = prev;
+			
+			current->next = prev;
+			prev->prev = current;
+
+			print_list(*list);
+			prev = current->prev;
+		}
+		current = next;
+	}
+}
